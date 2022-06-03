86bfe42a40ad59d609b8a594ebf016edd01c4524
Claire Thorington <claire.thorington@gmail.com>
2022-06-03 15:07:36 -0700

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -9,50 +9,7 @@
 //     const likeRed = $('input:radio[name=likeRed]:checked').val();
 //     const fearWorms = $('input:radio[name=fearWorms]:checked').val();
 
-    let score = 0;
-    let total = 6;
-    let point = 1;
-    let highest = total * point;
-
-    function init(Go){
-      userInput.howManyLet('two');
-      userInput.favLetter('g');
-      userInput.favThing('monopoly');
-      userInput.favMotion('move');
-    }
-
-    function init(Ruby){
-      userInput.howManyLet('four');
-      userInput.favLetter('r');
-      userInput.favThing('jewelry');
-      userInput.likeRed('yes');
-    }
-
-    function init(Python){
-      userInput.howManyLet('six');
-      userInput.favLetter('p');
-      userInput.favThing('reptiles');
-      userInput.favMotion('no');
-    }
-
-    $(document).ready(function (event) {
-      event.preventDefault();
-
-      function process(userInput) {
-        if(q == "Go"){
-          let submitted = $('input:radio[name=howManyLet]:checked').val();
-          if (submitted == "")
-        }
-      }
-
-    
-
-    let result;
-
-      
-      
-
-
+// let result;
 
     // if (howManyLet === 'two') {
     //   result = $('#go').show();
@@ -67,7 +24,48 @@
     //   result = $('#sorry').show();
     // }
 
+    // Working on a better scoring system below:
 
-    
-  })
-});
+    let score = 0;
+    let total = 4;
+    let point = 1;
+    let highest = total * point;
+
+    function init(Go){
+      userInput.setItem('howManyLet','two');
+      userInput.setItem('favLetter','g');
+      userInput.setItem('favThing','monopoly');
+      userInput.setItem('favMotion','move');
+    }
+
+    function init(Ruby){
+      userInput.setItem('howManyLet','four');
+      userInput.setItem('favLetter','r');
+      userInput.setItem('favThing','jewelry');
+      userInput.setItem('likeRed','yes');
+    }
+
+    function init(Python){
+      userInput.setItem('howManyLet','six');
+      userInput.setItem('favLetter','p');
+      userInput.setItem('favThing','reptiles');
+      userInput.setItem('fearWorms','no');
+    }
+
+    $(document).ready(function (event) {
+      event.preventDefault();
+
+      function process(userInput) {
+        let submitted = $('input:radio[name=howManyLet]:checked').val();
+          if (submitted == (Go)){
+            score++;
+          }
+          if (submitted == (Ruby)){
+            score++;
+          }
+          if (submitted == (Python)){
+            score++;
+          }
+          }
+
+        })
