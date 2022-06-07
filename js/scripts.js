ce50681f05352c030882aed6109aff109e3e203e
Claire Thorington <claire.thorington@gmail.com>
2022-06-07 09:57:10 -0700

    create initial files
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,10 @@
-$(document).ready(function () {
-  $("form#userInput").submit(function(event) {
-    event.preventDefault();
+// Business Logic
 
-    const howManyLet = $('input:radio[name=howManyLet]:checked').val();
-    const favLetter = $('input:radio[name=favLetter]:checked').val();
-    const favThing = $('input:radio[name=favThing]:checked').val();
-    const favMotion = $('input:radio[name=favMotion]:checked').val();
-    const likeRed = $('input:radio[name=likeRed]:checked').val();
-    const fearWorms = $('input:radio[name=fearWorms]:checked').val();
-
-    let result;
-
-    if (howManyLet === 'two') {
-      result = "You should consider learning GO!";
-    }
-      else if (howManyLet === 'four') {
-      result = "You should consider learning RUBY!";
-    }
-      else if (howManyLet === 'six') {
-      result = "You should consider learning PYTHON!"
-    }
-
-    $("#output").text(result);
-
+function wordCounter(text) {
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(word) {
+    wordCount++;
   });
-
-});
-
-    // There's a more complex scoring system below, but it's gonna be awhile before I get it working, and I don't even know if I'm on the right track. From what I've looked at today, it sounds like a loop might be a better solution than what's below.
-
-    // let score = 0;
-    // let total = 4;
-    // let point = 1;
-    // let highest = total * point;
-
-    // function init(Go){
-    //   userInput.setItem('howManyLet','two');
-    //   userInput.setItem('favLetter','g');
-    //   userInput.setItem('favThing','monopoly');
-    //   userInput.setItem('favMotion','move');
-    // }
-
-    // function init(Ruby){
-    //   userInput.setItem('howManyLet','four');
-    //   userInput.setItem('favLetter','r');
-    //   userInput.setItem('favThing','jewelry');
-    //   userInput.setItem('likeRed','yes');
-    // }
-
-    // function init(Python){
-    //   userInput.setItem('howManyLet','six');
-    //   userInput.setItem('favLetter','p');
-    //   userInput.setItem('favThing','reptiles');
-    //   userInput.setItem('fearWorms','no');
-    // }
-
-    // $(document).ready(function (event) {
-    //   event.preventDefault();
-
-    //   function process(userInput) {
-    //     let submitted = $('input:radio[name=howManyLet]:checked').val();
-    //       if (submitted == (Go)){
-    //         score++;
-    //       }
-    //       if (submitted == (Ruby)){
-    //         score++;
-    //       }
-    //       if (submitted == (Python)){
-    //         score++;
-    //       }
-    //       }
-
-    //     })
+  return wordCount;
+}
