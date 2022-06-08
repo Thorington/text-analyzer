25772eb139f10b288da6ec1e5e6f216058def027
Claire Thorington <claire.thorington@gmail.com>
2022-06-07 22:06:43 -0700

    add Ashe's updates
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,77 @@
+  
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
-}
+  }
   let wordCount = 0;
   const wordArray = text.split(" ");
   wordArray.forEach(function(element) {
     if (!Number(element)) {
-    wordCount++;
+      wordCount++;
     }
   });
   return wordCount;
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++
+      wordCount++;
     }
   });
   return wordCount;
 }
 
+// UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
+  });
+});
+
+// Business logic we want to save!!!
+
+// From Zachary!
 // function removeBadWords(words,text) {
 //   const noBadWords = text.split(" ");
 //   let i = 0;
@@ -40,17 +84,18 @@ function numberOfOccurrencesInText(word, text) {
 //   return noBadWords;
 // }
 
-function omitOffensiveWords(word, text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let goodtext = []
-  let textArray = text.split(" ");
-  textArray.forEach(function(element) {
-    if (!offensiveWords.includes(element.toLowerCase())) {
-      goodtext.push(element);
-    }
-  });
+// From Max!
+// function omitOffensiveWords(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let goodtext = []
+//   let textArray = text.split(" ");
+//   textArray.forEach(function(element) {
+//     if (!offensiveWords.includes(element.toLowerCase())) {
+//       goodtext.push(element);
+//     }
+//   });
 
-  return goodtext;
-}
+//   return goodtext;
+// }
